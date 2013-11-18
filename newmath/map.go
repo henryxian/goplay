083728618d2bd46293545a9a56906c364276b989
henryxian <henryxian@qq.com>
2013-11-19 00:26:50 +0800

    add map.go

---
+++ newmath/map.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    Lat, Long float64
+}
+
+var m map[string]Vertex
+var n map[int]Vertex
+
+func main() {
+    m = make(map[string]Vertex)
+    n = make(map[int]Vertex)
+    
+    m["Bell Labs"] = Vertex{
+        40.68433, -74.39967,
+    }
+    
+    n[1] = Vertex{ 20,  20,}
+    
+    fmt.Println(m["Bell Labs"])
+    fmt.Println(n[1])
+    
+}
